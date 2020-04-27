c0d765ef402054bc5bdc7a341cf9c64a4dab46ce
Fung <leungcheukfung12@gmail.com>
2020-04-28 01:57:03 +0800

    hi

--- app/src/main/java/com/example/sehh2279new/ShopActivityTest.java
+++ app/src/main/java/com/example/sehh2279new/ShopActivityTest.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ImageView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShopActivityTest extends AppCompatActivity {
+        private int value;
+        private double price;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,10 +19,22 @@ public class ShopActivityTest extends AppCompatActivity {
         setContentView(R.layout.shoppingcart);
         TextView quantitytext = findViewById(R.id.textView20);
         TextView pricetext = findViewById(R.id.textView25);
+        Button checkout = findViewById(R.id.button5);
         Bundle extras = getIntent().getExtras();
-        int value = extras.getInt("key");
+        value = extras.getInt("key");
         quantitytext.setText(Integer.toString(value));
-        //double price = 7.7 * quantity;
-        //pricetext.setText(price);
+        price = 7.7 * value;
+        pricetext.setText(Double.toString(price));
+        checkout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent i = new Intent(ShopActivityTest.this,payment.class);
+                i.putExtra("key1",Integer.toString(value));
+                i.putExtra("key2",Double.toString(price));
+                startActivity(i);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/sehh2279new/payment.java
@@ -0,0 +1,37 @@
+package com.example.sehh2279new;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class payment extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment);
+        TextView quantitytext2 = findViewById(R.id.textView3);
+        TextView pricetext2 = findViewById(R.id.textView5);
+        Button confirm = findViewById(R.id.button);
+        Bundle extras = getIntent().getExtras();
+        String value = extras.getString("key1");
+        quantitytext2.setText(value);
+        String price = extras.getString("key2");
+        pricetext2.setText(price);
+        confirm.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent i = new Intent(payment.this,ConfirmationActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+    }
+}
