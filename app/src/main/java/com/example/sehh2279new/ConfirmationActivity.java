d5bd102306c5edd3d7622e770d5f68b0cf003234
wl <lsklosiukit@gmail.com>
2020-04-30 13:13:58 +0800

    click to toggle

--- app/src/main/java/com/example/sehh2279new/ConfirmationActivity.java
+++ app/src/main/java/com/example/sehh2279new/ConfirmationActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class ConfirmationActivity extends AppCompatActivity {
+public class ConfirmationActivity extends AppCompatActivity implements View.OnClickListener{
+    boolean show = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +17,30 @@ public class ConfirmationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_confirmation);
 
         Button button = findViewById(R.id.button_okay);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        button.setOnClickListener(this);
+
+        TextView tv_test = findViewById(R.id.textView13);
+        tv_test.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case (R.id.button_okay):
                 Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
-            }
-        });
+                break;
+            case (R.id.textView13):
+                TextView tv_test = findViewById(R.id.textView13);
+                if(!show){
+                    tv_test.setText("DLLM");
+                    show = true;
+                } else {
+                    tv_test.setText("ORIGINAL");
+                    show = false;
+                }
+                break;
+        }
     }
 }
