f6a330f3d2e707b1fb1b0401cb6e47f4dc9f010e
wl <lsklosiukit@gmail.com>
2020-05-04 02:19:04 +0800

    Final?

--- app/src/main/java/com/example/sehh2279new/HomeFragment.java
+++ app/src/main/java/com/example/sehh2279new/HomeFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +21,13 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        LinearLayout home = view.findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Function not implemented", Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 
