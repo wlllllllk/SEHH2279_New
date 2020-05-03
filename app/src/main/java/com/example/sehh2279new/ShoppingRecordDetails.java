d0197d851b6063075a8836afb3913634c3fb1340
ChungWaiChun <jackychung02@gmail.com>
2020-05-03 21:35:06 +0800

    lsk on99

---
+++ app/src/main/java/com/example/sehh2279new/ShoppingRecordDetails.java
@@ -0,0 +1,14 @@
+package com.example.sehh2279new;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoppingRecordDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_record_details);
+    }
+}
--- app/src/main/java/com/example/sehh2279new/ShoppingRecordsActivity.java
+++ app/src/main/java/com/example/sehh2279new/ShoppingRecordsActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ShoppingRecordsActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +17,20 @@ public class ShoppingRecordsActivity extends AppCompatActivity {
 
         this.setTitle("Shopping Records");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        addListenerOnbutton();
     }
 
-    @Override
+
+    public void addListenerOnbutton(){
+        button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ShoppingRecordsActivity.this, ShoppingRecordDetails.class);
+                startActivity(intent);
+            }
+        });
+    }
     public boolean onSupportNavigateUp() {
         finish();
         return true;
