88404ae3b7dfc50f99ed30db9c638d0a28c52de5
wl <lsklosiukit@gmail.com>
2020-05-03 17:14:55 +0800

    Fruit Old

--- app/src/main/java/com/example/sehh2279new/ShopFragment.java
+++ app/src/main/java/com/example/sehh2279new/ShopFragment.java
@@ -8,13 +8,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
 
 public class ShopFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().setTitle("Shop");
+        View view = inflater.inflate(R.layout.fragment_shop, container, false);
 
-        return inflater.inflate(R.layout.fragment_shop, container, false);
+        return view;
     }
 }
