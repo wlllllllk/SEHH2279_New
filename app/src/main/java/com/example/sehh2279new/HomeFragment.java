cd4a48bf3854f80c63edcc89ccd4d4609ecf8a4a
wl <lsklosiukit@gmail.com>
2020-05-03 16:55:11 +0800

    Fruit Old

--- app/src/main/java/com/example/sehh2279new/HomeFragment.java
+++ app/src/main/java/com/example/sehh2279new/HomeFragment.java
@@ -20,65 +20,6 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        Button button = (Button) view.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                /*Fragment fragment = new ProductDetailsFragment();
-                FragmentManager fragmentManager = getFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_container, fragment);
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();*/
-
-                Intent i = new Intent(getActivity(), ProductDetailsActivity.class);
-                startActivity(i);
-
-            }
-        });
-
-        Button button1 = (Button) view.findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-
-                Intent i = new Intent(getActivity(), ConfirmationActivity.class);
-                startActivity(i);
-
-            }
-        });
-
-        Button button6 = (Button) view.findViewById(R.id.button6);
-        button6.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-
-                Intent i = new Intent(getActivity(), ShopActivityTest.class);
-                startActivity(i);
-
-            }
-        });
-
-        Button button8 = (Button) view.findViewById(R.id.button8);
-        button8.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-
-                Intent i = new Intent(getActivity(), SortFilterActivity.class);
-                startActivity(i);
-
-            }
-        });Button button10 = (Button) view.findViewById(R.id.button10);
-        button10.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-
-                Intent i = new Intent(getActivity(), LoginActivity.class);
-                startActivity(i);
-
-            }
-        });
-
         return view;
     }
 
