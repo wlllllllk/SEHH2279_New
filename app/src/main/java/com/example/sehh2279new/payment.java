2f5c0978ef3b8abc46325eb8a272cc520eb903e5
Fung <leungcheukfung12@gmail.com>
2020-05-03 22:56:55 +0800

    hi

--- app/src/main/java/com/example/sehh2279new/payment.java
+++ app/src/main/java/com/example/sehh2279new/payment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -18,7 +19,9 @@ public class payment extends AppCompatActivity {
 
         this.setTitle("Checkout");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Button confirm = findViewById(R.id.button);
+        ImageView x = findViewById(R.id.imageView27);
+        ImageView y = findViewById(R.id.imageView32);
+        ImageView z = findViewById(R.id.imageView33);
         /*TextView quantitytext2 = findViewById(R.id.textView86);
         TextView pricetext2 = findViewById(R.id.textView84);
         TextView totaltext2 = findViewById(R.id.textView92);
@@ -29,15 +32,26 @@ public class payment extends AppCompatActivity {
         pricetext2.setText(price);
         String total = extras.getString("key3");
         totaltext2.setText("$ "+total);*/
-        confirm.setOnClickListener(new View.OnClickListener(){
+        x.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-
                 Intent i = new Intent(payment.this,ConfirmationActivity.class);
                 startActivity(i);
-
             }
         });
-
+        y.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(payment.this,ConfirmationActivity.class);
+                startActivity(i);
+            }
+        });
+        z.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(payment.this,ConfirmationActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
