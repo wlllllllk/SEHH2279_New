a52f61204343be22c427bc24f3cb7d7b6cc8df45
ChungWaiChun <jackychung02@gmail.com>
2020-05-03 18:56:46 +0800

    lsk on99

--- app/src/main/java/com/example/sehh2279new/SortFilterActivity.java
+++ app/src/main/java/com/example/sehh2279new/SortFilterActivity.java
@@ -2,10 +2,13 @@ package com.example.sehh2279new;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SortFilterActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,11 +16,23 @@ public class SortFilterActivity extends AppCompatActivity {
 
         this.setTitle("Sort & Filter");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        addListenerOnButton();
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         finish();
         return true;
+    }
+    public void addListenerOnButton() {
+        button = (Button) findViewById(R.id.button);//declare which button will be called
+        //prepare button to be ready when clicked
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SortFilterActivity.this, FilterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
